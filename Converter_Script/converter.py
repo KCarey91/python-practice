fc38fb34eba7d194afe480dc47695331f2d02224
Kevin Carey <37804348+KCarey91@users.noreply.github.com>
2022-06-08 10:12:37 +0100

    adding try catch and constants

--- Converter_Script/converter.py
+++ Converter_Script/converter.py
@@ -1,24 +1,29 @@
 import sys
 import time
 
+FEET_TO_MM_CONVERSION = 304.8
+MM_TO_FEET_CONVERSION = 0.0032808
+CELSIUS_CONVERSION = 1.8
+FAHRENHEIT_CONVERSION = 32
+
 
 def feet_to_mm(feet):
-    conversion_to_mm = feet * 304.8
+    conversion_to_mm = feet * FEET_TO_MM_CONVERSION
     return conversion_to_mm
 
 
 def mm_to_feet(mm):
-    conversion_to_feet = mm * 0.0032808
+    conversion_to_feet = mm * MM_TO_FEET_CONVERSION
     return conversion_to_feet
 
 
 def celsius_to_fahrenheit(c):
-    fahrenheit = (c * 1.8) + 32
+    fahrenheit = (c * CELSIUS_CONVERSION) + FAHRENHEIT_CONVERSION
     return fahrenheit
 
 
 def fahrenheit_to_celsius(f):
-    celsius = (f - 32) / 1.8
+    celsius = (f - FAHRENHEIT_CONVERSION) / CELSIUS_CONVERSION
     return celsius
 
 
@@ -48,49 +53,54 @@ def menu():
 
         except ValueError:
              print("Error, This is not a number. Try again.")
+             menu()
 
         else:
-            if selection == 1:
-                feet_total = float(input("Enter the amount of feet you would like converted to mm, you can use a decimal point to represent inches ex: 12.6: "))
-                total = feet_to_mm(feet_total)
-                # Format result to one decimal point
-                formatted_total = round(total, 1)
-                print(f"\n{feet_total}ft is {formatted_total}mm\n")
-                # Sleep and show the menu again
-                time.sleep(2)
-                menu()
-            elif selection == 2:
-                mm_total = float(input("Enter the amount of mm you would like converted to feet: "))
-                total = mm_to_feet(mm_total)
-                # Format result to one decimal point
-                formatted_total = round(total, 1)
-                print(f"{mm_total}mm is {formatted_total}ft")
-                # Sleep and show the menu again
-                time.sleep(1)
-                menu()
-            elif selection == 3:
-                celsius = float(input("Temperature value in degree Celsius: "))
-                total = celsius_to_fahrenheit(celsius)
-                # Format result to one decimal point
-                formatted_total = round(total, 2)
-                print(f"{celsius} degrees celsius is {formatted_total} degrees Fahrenheit")
-                # Sleep and show the menu again
-                time.sleep(1)
-                menu()
-            elif selection == 4:
-                fahrenheit = float(input("Temperature value in degree Fahrenheit: "))
-                total = fahrenheit_to_celsius(fahrenheit)
-                # Format result to one decimal point
-                formatted_total = round(total, 2)
-                print(f"{fahrenheit} degrees fahrenheit is {formatted_total} degress celsius")
-                # Sleep and show the menu again
-                time.sleep(1)
-                menu()
-            elif selection == 5:
-                quit()
-            else:
-                print("You did not select a valid option")
-                time.sleep(1)
+            try:
+                if selection == 1:
+                    feet_total = float(input("Enter the amount of feet you would like converted to mm, you can use a decimal point to represent inches ex: 12.6: "))
+                    total = feet_to_mm(feet_total)
+                    # Format result to one decimal point
+                    formatted_total = round(total, 1)
+                    print(f"\n{feet_total}ft is {formatted_total}mm\n")
+                    # Sleep and show the menu again
+                    time.sleep(2)
+                    menu()
+                elif selection == 2:
+                    mm_total = float(input("Enter the amount of mm you would like converted to feet: "))
+                    total = mm_to_feet(mm_total)
+                    # Format result to one decimal point
+                    formatted_total = round(total, 1)
+                    print(f"{mm_total}mm is {formatted_total}ft")
+                    # Sleep and show the menu again
+                    time.sleep(1)
+                    menu()
+                elif selection == 3:
+                    celsius = float(input("Temperature value in degree Celsius: "))
+                    total = celsius_to_fahrenheit(celsius)
+                    # Format result to one decimal point
+                    formatted_total = round(total, 2)
+                    print(f"{celsius} degrees celsius is {formatted_total} degrees Fahrenheit")
+                    # Sleep and show the menu again
+                    time.sleep(1)
+                    menu()
+                elif selection == 4:
+                    fahrenheit = float(input("Temperature value in degree Fahrenheit: "))
+                    total = fahrenheit_to_celsius(fahrenheit)
+                    # Format result to one decimal point
+                    formatted_total = round(total, 2)
+                    print(f"{fahrenheit} degrees fahrenheit is {formatted_total} degress celsius")
+                    # Sleep and show the menu again
+                    time.sleep(1)
+                    menu()
+                elif selection == 5:
+                    quit()
+                else:
+                    print("You did not select a valid option")
+                    time.sleep(1)
+                    menu()
+            except ValueError:
+                print("Error, This is not a number. Try again.")
                 menu()
         break
 
